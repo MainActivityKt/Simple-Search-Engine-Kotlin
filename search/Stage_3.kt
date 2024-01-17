796da107b05277c3df7c494b62f95c5b8f11ae1e
Safi <Safiullahkakar20202@outlook.com>
2024-01-17 17:07:33 +0430

    Added Stage 3 solution code

---
+++ search/Stage_3.kt
@@ -0,0 +1,86 @@
+package search
+
+import kotlin.system.exitProcess
+
+
+enum class Titles(val title: String) {
+    MENU("Menu"),
+    LIST_PEOPLE("List of people")
+
+}
+
+
+class SearchEngineWithMenu() {
+    private lateinit var input: String
+    private var data: List<String> = getInputs()
+    private var menu = StringBuilder().apply {
+        appendLine("=== ${Titles.MENU.title} ===")
+        appendLine("1. Find a person")
+        appendLine("2. Print all people")
+        append("0. Exit")
+    }
+
+
+    fun start() {
+        printMenu()
+        getNewInput()
+        while (true) {
+            processInput()
+            printMenu()
+            getNewInput()
+        }
+    }
+
+    private fun getNewInput() {
+        input = readln()
+    }
+
+    private fun processInput() {
+        val option = input.toIntOrNull()
+        println()
+        if (option != null && option in 0..2) {
+            when (option) {
+                1 -> performSearch()
+                2 -> printAllData()
+                0 -> {
+                    println("Bye!")
+                    exitProcess(0)
+                }
+            }
+
+        } else {
+            println("Incorrect option! Try again.")
+        }
+    }
+
+    private fun printAllData() {
+        val result = StringBuilder().apply {
+            appendLine("=== ${Titles.LIST_PEOPLE.title} ===")
+            data.forEach { appendLine(it) }
+        }
+        println(result)
+    }
+
+    private fun performSearch() {
+        println("Enter a name or email to search all suitable people.")
+        val keyword = readln()
+        data.forEach {
+            if (it.contains(keyword, ignoreCase = true)) {
+                println(it)
+            }
+        }
+        println()
+    }
+
+
+    private fun printMenu() {
+        println(menu)
+    }
+}
+
+
+
+fun main() {
+    val searchEngine = SearchEngineWithMenu()
+    searchEngine.start()
+}
